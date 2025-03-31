9deb060529ae18d8982e2379e76fe014d95d343c
KumarSamarth-ofc <kumarsamarth.adept@gmail.com>
2025-03-31 22:04:42 +0530

    ignoreUndefinedProperties in firestore

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 
 import { getAuth } from "firebase/auth";
-import { getFirestore } from  "firebase/firestore"
+import { getFirestore, initializeFirestore } from  "firebase/firestore"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,4 +19,4 @@ const firebaseConfig = {
 const app = !getApps.length ? initializeApp(firebaseConfig):getApp();
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, { ignoreUndefinedProperties: true });
