cf716fa34a6ec7031545f1f2e5813d24aae277e8
KumarSamarth-ofc <kumarsamarth.adept@gmail.com>
2025-03-31 13:04:03 +0530

    first commit

---
+++ firebase/client.ts
@@ -0,0 +1,22 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+
+import { getAuth } from "firebase/auth";
+import { getFirestore } from  "firebase/firestore"
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8YYjE292qQEWFJTpliDW7KoyKcesffpQ",
+  authDomain: "prepwise-8fb8c.firebaseapp.com",
+  projectId: "prepwise-8fb8c",
+  storageBucket: "prepwise-8fb8c.firebasestorage.app",
+  messagingSenderId: "170049566366",
+  appId: "1:170049566366:web:a0fa47f50cb6418862ed8f",
+  measurementId: "G-CW1VXEN8BD",
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig):getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
